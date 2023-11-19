ad7cb1b9f2fa67da9251a7b0ca8799a09eba5a37
Jorge Enrique Gonzales Carrión <104078975+JorgeGonzales15@users.noreply.github.com>
2023-11-19 17:20:05 -0500

    fix: added deploy cors

--- src/main/java/pe/com/AutoPago/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/pe/com/AutoPago/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -16,7 +16,7 @@ public class OpenApiConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://autopagoapp-6df05.web.app")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
